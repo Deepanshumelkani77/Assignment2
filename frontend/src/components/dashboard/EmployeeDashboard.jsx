04b2290e19302e20cb91e0a251cdb637581a31ab
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-12-05 20:52:23 +0530

    try to make UI

--- frontend/src/components/dashboard/EmployeeDashboard.jsx
+++ frontend/src/components/dashboard/EmployeeDashboard.jsx
@@ -163,7 +163,7 @@ const EmployeeDashboard = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-200">
       {/* Header Navbar */}
       <nav className="bg-white border-b border-gray-200 shadow-sm">
         <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
