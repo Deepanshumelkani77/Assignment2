d12d7ad9ef90df60e826494e366b547dfdc90069
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-12-05 23:21:18 +0530

    done

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 
-const API_URL = 'http://localhost:7000';
+const API_URL = 'https://assignment2-t9he.onrender.com';
 
 export const AppContext = createContext();
 
